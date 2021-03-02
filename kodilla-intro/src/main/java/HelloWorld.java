7fb5d26ea1fa919835988283b964b0f6336c59f2
Dagmara Kulik <dagmara.kulik7@gmail.com>
2021-03-02 14:01:52 +0100

    First task

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Dagmara");
+        System.out.println(24);
+        System.out.println('K');
+    }
+}
