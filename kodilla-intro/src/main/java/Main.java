2211ac4c0092c0642fe502d674f34bac9ec78b9c
Dagmara Kulik <dagmara.kulik7@gmail.com>
2021-03-02 16:01:11 +0100

    FirstClass Modification and new MainExtended class

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String [] args) {
+        String myName = "Dagmara";
+        int myAge = 24;
+        double height = 1.70;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String myCar = "Kia";
+        int productionYear = 2017;
+        double averageFuelUsage = 7.5;
+        char carClass = 'C';
+        boolean isYourFirstCar = false;
+        String carModel = "Ceed";
+        System.out.println(myCar);
+        System.out.println(carModel);
+        System.out.println(carClass);
+        System.out.println(productionYear);
+        System.out.println(averageFuelUsage);
+        System.out.println(isYourFirstCar);
+    }
+}
