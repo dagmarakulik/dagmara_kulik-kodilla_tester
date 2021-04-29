651e1827daf62e30c00ba8c07d9cb37eab3797f3
Dagmara Kulik <dagmara.kulik7@gmail.com>
2021-04-29 17:54:37 +0200

    Task 9_1 - Delivery And Notification Service

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/SMSNotificationService.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.basic.dependency_injection.homework;
+
+public class SMSNotificationService implements NotificationService {
+
+    @Override
+    public void success(String address) {
+        System.out.println("Sms: Package delivered to: " + address);
+    }
+
+    @Override
+    public void fail(String address) {
+        System.out.println("Sms: Package not delivered to: " + address);
+    }
+}
