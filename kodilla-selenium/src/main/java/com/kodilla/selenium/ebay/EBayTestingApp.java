66f0a3cd81f2f248080509c1bea8e85b05c092dd
Dagmara Kulik <dagmara.kulik7@gmail.com>
2021-06-07 13:04:29 +0200

    Task 13_3 - EbayTestingApp (Selenium Webdriver)

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -0,0 +1,20 @@
+package com.kodilla.selenium.ebay;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EBayTestingApp {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/Dagmara/Documents/Development/Kodilla/chromedriver");      // [1]
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com");
+
+        WebElement inputField = driver.findElement(By.name("_nkw"));
+        inputField.sendKeys("Laptop");
+        inputField.submit();
+
+    }
+}
