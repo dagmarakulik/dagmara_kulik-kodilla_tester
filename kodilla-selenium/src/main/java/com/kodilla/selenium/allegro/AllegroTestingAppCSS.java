c059c8c94b9e51c8e0678222aae85326ecb67ea0
Dagmara Kulik <dagmara.kulik7@gmail.com>
2021-06-22 23:50:45 +0200

    Task 13.5 - cssSelector

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCSS.java
@@ -0,0 +1,33 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class AllegroTestingAppCSS {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/Dagmara/Documents/Development/Kodilla/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.allegro.pl/");
+
+        WebElement alert = driver.findElement(By.cssSelector("div>button[class*='m7er_0k']"));
+        alert.click();
+
+        WebElement category = driver.findElement(By.cssSelector("div>select[class*='mr3m_1']"));
+        Select categorySelect = new Select(category);
+        categorySelect.selectByIndex(3);
+
+        WebElement searchItem = driver.findElement(By.cssSelector("div>form>input"));
+        searchItem.sendKeys("Mavic mini");
+        searchItem.submit();
+
+        List<WebElement> listOfProducts = driver.findElements(By.cssSelector("section>article"));
+        for (WebElement product : listOfProducts) {
+            System.out.println(product.getText());
+        }
+    }
+}
